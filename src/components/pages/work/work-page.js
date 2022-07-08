a1c2369495b94b7a2269665acbfc5b3c7f0df6cd
RomanDovgii <romandovgiiwork@gmail.com>
2022-07-08 22:23:36 +0300

    Добавляет страницу одной работы

--- src/components/pages/work/work-page.js
+++ src/components/pages/work/work-page.js
@@ -1,10 +1,12 @@
+import HeadingOne from "../../styled-blocks/text-parts/heading-one/heading-one";
+import Paragraph from "../../styled-blocks/text-parts/paragraph/paragraph";
 import Wrapper from "../../styled-blocks/wrapper/wrapper";
 
 function WorkPage() {
     return <Wrapper>
-        {/* Work name */}
-        {/* Work description */}
-        {/* List of similar works */}
+        <Cover></Cover>
+        <HeadingOne></HeadingOne>
+        <Paragraph></Paragraph>
     </Wrapper>
 }
 
